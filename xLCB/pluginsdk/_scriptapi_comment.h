97a02065e328405d53d029da147c6608f5a8773c
mrexodia <mr.exodia.tpodt@gmail.com>
2016-10-02 14:46:07 +0200

    better gitignore to compile out of the box

--- xLCB/pluginsdk/_scriptapi_comment.h
+++ xLCB/pluginsdk/_scriptapi_comment.h
@@ -5,25 +5,25 @@
 
 namespace Script
 {
-namespace Comment
-{
-struct CommentInfo
-{
-    char mod[MAX_MODULE_SIZE];
-    duint rva;
-    char text[MAX_LABEL_SIZE];
-    bool manual;
-};
+    namespace Comment
+    {
+        struct CommentInfo
+        {
+            char mod[MAX_MODULE_SIZE];
+            duint rva;
+            char text[MAX_LABEL_SIZE];
+            bool manual;
+        };
 
-SCRIPT_EXPORT bool Set(duint addr, const char* text, bool manual = false);
-SCRIPT_EXPORT bool Set(const CommentInfo* info);
-SCRIPT_EXPORT bool Get(duint addr, char* text); //text[MAX_COMMENT_SIZE]
-SCRIPT_EXPORT bool GetInfo(duint addr, CommentInfo* info);
-SCRIPT_EXPORT bool Delete(duint addr);
-SCRIPT_EXPORT void DeleteRange(duint start, duint end);
-SCRIPT_EXPORT void Clear();
-SCRIPT_EXPORT bool GetList(ListOf(CommentInfo) list); //caller has the responsibility to free the list
-}; //Comment
+        SCRIPT_EXPORT bool Set(duint addr, const char* text, bool manual = false);
+        SCRIPT_EXPORT bool Set(const CommentInfo* info);
+        SCRIPT_EXPORT bool Get(duint addr, char* text); //text[MAX_COMMENT_SIZE]
+        SCRIPT_EXPORT bool GetInfo(duint addr, CommentInfo* info);
+        SCRIPT_EXPORT bool Delete(duint addr);
+        SCRIPT_EXPORT void DeleteRange(duint start, duint end);
+        SCRIPT_EXPORT void Clear();
+        SCRIPT_EXPORT bool GetList(ListOf(CommentInfo) list); //caller has the responsibility to free the list
+    }; //Comment
 }; //Script
 
 #endif //_SCRIPTAPI_COMMENT_H
